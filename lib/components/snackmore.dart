31323b9d4b18e0c0022cac95696a425964cf8f72
bhangun <bhangunh@gmail.com>
2018-10-08 16:34:58 +0700

    jadinya pake untuk menu yg muncul dari bawah showModalBottomSheet

--- lib/components/snackmore.dart
+++ lib/components/snackmore.dart
@@ -9,15 +9,21 @@ class Snackmenu extends StatelessWidget {
         IconButton(
             icon: Image.asset(GO_MORE_ICON),
             onPressed: () {
-              Navigator.of(context, rootNavigator: true).push(
-                new MaterialPageRoute<bool>(
+              showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return new Container(
+                        child: new Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: new Text(
+                                'This is the modal bottom sheet. Click anywhere to dismiss.',
+                                textAlign: TextAlign.center,
+                                style: new TextStyle(
+                                    color: Theme.of(context).accentColor,
+                                    fontSize: 24.0))));
+                  });
+            }),
 
-                  fullscreenDialog: false,
-                  builder: (BuildContext context) => new Tab3Dialog(),
-                ),
-              );
-            }
-            ),
         Text(
           GO_MORE,
           style: TextStyle(color: Colors.white),
@@ -27,24 +33,18 @@ class Snackmenu extends StatelessWidget {
   }
 }
 
-Widget _menu(){
+Widget _menu() {
   return Column(
-    children:<Widget>[
-      _banner()
-
-    ] ,
+    children: <Widget>[_banner()],
   );
 }
-
 
 Widget _banner() {
   return Container(
     padding: EdgeInsets.only(top: 10.0),
     height: 150.0,
     child: Column(
-      children: <Widget>[
-
-      ],
+      children: <Widget>[],
     ),
     decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5.0),
@@ -54,23 +54,21 @@ Widget _banner() {
   );
 }
 
-
-
-
 class Tab3Dialog extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return new Column(
-  children: <Widget>[
-    Text("bibibibib"),
-    FlatButton(
-      child:  Text('Cancel'),
-      padding: EdgeInsets.zero,
-      onPressed: () {
-        Navigator.of(context).pop(false);
-      },
-    )
-  ],
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.only(top: 300.0)),
+        Text("bibibibib"),
+        FlatButton(
+          child: Text('Cancel'),
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+        )
+      ],
+    );
+  }
 }
