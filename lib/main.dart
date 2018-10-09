3efe6266c7dec4e4e5b26265bfcef7f4573cdd40
bhangun <bhangunh@gmail.com>
2018-10-09 22:49:05 +0700

    tambah splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gojekdummy/home.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -13,7 +14,21 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
         fontFamily: 'NeoSansPro'
       ),
-      home: new MyHomePage(title: 'Gojek'),
+      home: SplashScreen(
+            seconds: 5,
+            navigateAfterSeconds: MyHomePage(title: 'Gojek'),
+            title: new Text('Welcome In SplashScreen',
+              style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0
+              ),),
+            image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+            backgroundColor: Colors.white,
+            styleTextUnderTheLoader: new TextStyle(),
+            photoSize: 100.0,
+            onClick: ()=>print("Flutter Egypt"),
+            loaderColor: Colors.red
+        )
     );
   }
 }
